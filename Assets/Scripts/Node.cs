8f1d27b56335a4a0e60a6514af652b64407193c9
Igor <76399365+Igi-Desu@users.noreply.github.com>
2022-12-28 14:34:01 +0100

    better pathfinding

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,10 +1,14 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// represents node from map
+/// </summary>
 public class Node : MonoBehaviour
 {
     public PathNode pathnode;
+    /// <summary>
+    /// represents a single node connected to current one 
+    /// </summary>
     [System.Serializable]
     public struct NeighbourNode
     {
@@ -20,36 +24,50 @@ public class Node : MonoBehaviour
             return "Node with cost of " + dist;
         }
     }
-     public List<NeighbourNode> neightbourNodes { get; private set; } = new List<NeighbourNode>();
+    /// <summary>
+    /// lst of all nodes connect to current one 
+    /// </summary>
+    public List<NeighbourNode> neightbourNodes { get; private set; } = new List<NeighbourNode>();
+    /// <summary>
+    /// List of vectors representing possible turns from current node
+    /// all vectors contained are simple directions left right up down
+    /// </summary>
     public List<Vector2> whereWeCanGo { get; private set; }
-    [SerializeField] LayerMask collisionLayer;
+    /// <summary>
+    /// represents walls - objects that we can't go through
+    /// </summary>
+    [SerializeField] LayerMask walls;
     [SerializeField] LayerMask nodeMask;
     protected void Start()
     {
-
-        //add Node to pathfinding node list 
         whereWeCanGo = new List<Vector2>();
-        Invoke("checkpositions", 0.5f);
+        //invoke checkpositions after a moment so that all nodes already got their
+        //start method finished
+        Invoke("CheckPositions", 0.5f);
     }
-    void checkpositions()
+    /// <summary>
+    /// Checks all possible directions looking for neighbours
+    /// </summary>
+    void CheckPositions()
     {
         CheckPos(Vector2.up);
         CheckPos(Vector2.right);
         CheckPos(Vector2.down);
         CheckPos(Vector2.left);
         pathnode = new PathNode(this);
-
-        //after we check position
     }
+    /// <summary>
+    /// Checks for neighbour and adds it to neighbour list if found
+    /// </summary>
+    /// <param name="direction">direction in which we are looking for a neighbour</param>
     void CheckPos(Vector2 direction)
     {
-        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * .75f, .0f, direction, 1.5f, collisionLayer);
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * .75f, .0f, direction, 1.5f, walls);
         //means we can go this way so there should be node there
         if (hit.collider == null)
         {
             //add node and calculate the cost(distance) between them
             RaycastHit2D nodehit = Physics2D.Raycast(gameObject.transform.position + (Vector3)direction, direction, 15f, nodeMask);
-            //if there's no node in that direction just return
             if (nodehit.collider == null)
             {
                 return;
@@ -57,26 +75,18 @@ public class Node : MonoBehaviour
             whereWeCanGo.Add(direction);
             //else add that node into neighbours list 
             Node n = nodehit.transform.GetComponent<Node>();
-            NeighbourNode node = new NeighbourNode(n, getcost(this, n));
+            NeighbourNode node = new NeighbourNode(n, CalculateDistance(this, n));
             neightbourNodes.Add(node);
         }
     }
-    int getcost(Node a, Node b)
+    /// <summary>
+    /// calculates distance between two given nodes
+    /// it calculates manhattan distance since we can't diagonally
+    /// </summary>
+    public static int CalculateDistance(Node a, Node b)
     {
         Vector2 dist = a.transform.position - b.transform.position;
         int distance = (int)Mathf.Abs(dist.x + dist.y);
-        // Debug.Log($" vectors  {a.transform.position} , {b.transform.position}");
-        // Debug.Log(  $"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA {distance}");
         return distance;
     }
-    // private void OnDrawGizmos() {
-    //     Gizmos.DrawRay(new Ray(transform.position,Vector2.up));
-    //      Gizmos.DrawRay(new Ray(transform.position,Vector2.down));
-    //      Gizmos.DrawRay(new Ray(transform.position,Vector2.right));
-    //      Gizmos.DrawRay(new Ray(transform.position,Vector2.left));
-    // }
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.DrawCube(transform.position + Vector3.left, Vector3.one * .9f);
-    //}
 }
--- Assets/Scripts/PathNode.cs
+++ Assets/Scripts/PathNode.cs
@@ -1,16 +1,33 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
+/// <summary>
+/// represents a pathnode used in A* pathfinding algorithm
+/// </summary>
 public class PathNode {
+    /// <summary>
+    /// Represents Node object that this pathnode is attached to
+    /// </summary>
     public Node node;
-    //heuristic cost
-    public int hcost= int.MaxValue;
-    //get to cost
-    public int gcost= int.MaxValue;
-    //expected cost (hcost+gcost)
-    public int fcost= int.MaxValue;
-    public PathNode fromwhere=null;
+    /// <summary>
+    /// heuristic cost of getting to end node from this one
+    /// </summary>
+    public int hCost= int.MaxValue;
+    /// <summary>
+    /// Real cost of getting from start node to this one 
+    /// </summary>
+    public int gCost= int.MaxValue;
+    /// <summary>
+    /// gcost + fcost -> the lower, the higher chance way is better according to A*
+    /// </summary>
+    public int fCost= int.MaxValue;
+    /// <summary>
+    /// From which pathnode we got to this one
+    /// </summary>
+    public PathNode fromWhere=null;
+    /// <summary>
+    /// direction from which we got to this node
+    /// </summary>
     public Vector2 fromwheredir=Vector2.zero;
     public PathNode(Node node){
             this.node=node;
